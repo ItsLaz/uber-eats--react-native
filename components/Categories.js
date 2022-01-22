df339a22ca5fab4439b5a961b73f84803c9079a3
ItsLaz <yesimandriy@gmail.com>
2022-01-22 00:05:17 -0600

    categories component

--- components/Categories.js
+++ components/Categories.js
@@ -1,10 +1,67 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 import React from 'react';
+
+const items = [
+    {
+        image: require('../assets/images/shopping-bag.png'),
+        text: 'Pick-up',
+    },
+    {
+        image: require('../assets/images/soft-drink.png'),
+        text: 'Soft Drinks',
+    },
+    {
+        image: require('../assets/images/bread.png'),
+        text: 'Bakery Items',
+    },
+    {
+        image: require('../assets/images/fast-food.png'),
+        text: 'Fast Foods',
+    },
+    {
+        image: require('../assets/images/deals.png'),
+        text: 'Deals',
+    },
+    {
+        image: require('../assets/images/coffee.png'),
+        text: 'Coffee & Tea',
+    },
+    {
+        image: require('../assets/images/desserts.png'),
+        text: 'Desserts',
+    },
+];
 
 const Categories = () => {
     return (
-        <View>
-            <Text>Categories</Text>
+        <View
+            style={{
+                margiTop: 5,
+                backgroundColor: 'white',
+                paddingVertical: 10,
+                paddingLeft: 20,
+            }}
+        >
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {items.map((item, index) => (
+                    <View
+                        style={{ alignItems: 'center', marginRight: 30 }}
+                        key={index}
+                    >
+                        <Image
+                            source={item.image}
+                            style={{
+                                width: 50,
+                                height: 40,
+                                resizeMode: 'contain',
+                            }}
+                        />
+                        <Text style={{ fontSize: 13, fontWeight: 'bold' }}>
+                            {item.text}
+                        </Text>
+                    </View>
+                ))}
+            </ScrollView>
         </View>
     );
 };
