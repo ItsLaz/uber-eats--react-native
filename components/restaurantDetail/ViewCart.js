9beca198194f94822ad870f0d3e020b69fab3cfa
ItsLaz <yesimandriy@gmail.com>
2022-01-29 16:54:24 -0600

    cart modal

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,9 +1,14 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import OrderItem from './OrderItem';
 
 const ViewCart = () => {
-    const items = useSelector((state) => state.cartReducer.selectedItems.items);
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const { items, restaurantName } = useSelector(
+        (state) => state.cartReducer.selectedItems
+    );
     const total = items
         .map((item) => Number(item.price.replace('$', ' ')))
         .reduce((prev, curr) => prev + curr, 0)
@@ -14,8 +19,87 @@ const ViewCart = () => {
         currency: 'USD',
     });
 
+    const styles = StyleSheet.create({
+        modalContainer: {
+            flex: 1,
+            justifyContent: 'flex-end',
+            backgroundColor: 'rgba(0,0,0,0.7)',
+        },
+        modalCheckoutContainer: {
+            backgroundColor: 'white',
+            padding: 16,
+            height: 500,
+            borderWidth: 1,
+        },
+        restaurantName: {
+            textAlign: 'center',
+            fontWeight: 'bold',
+            fontSize: 18,
+            marginBottom: 10,
+        },
+        subtotalContainer: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginTop: 5,
+            padding: 20,
+        },
+        subtotalText: {
+            textAlign: 'left',
+            fontWeight: 'bold',
+            fontSize: 15,
+            marginBottom: 10,
+        },
+    });
+
+    const checkoutModalContent = () => (
+        <>
+            <View style={styles.modalContainer}>
+                <View style={styles.modalCheckoutContainer}>
+                    <Text style={styles.restaurantName}>{restaurantName}</Text>
+                    {items.map((item, index) => (
+                        <OrderItem key={index} item={item} />
+                    ))}
+                    <View style={styles.subtotalContainer}>
+                        <Text style={styles.subtotalText}>Subtotal</Text>
+                        <Text>${totalUSD}</Text>
+                    </View>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                backgroundColor: 'black',
+                                alignItems: 'center',
+                                padding: 13,
+                                borderRadius: 30,
+                                width: 300,
+                                position: 'relative',
+                            }}
+                            onPress={() => setModalVisible(false)}
+                        >
+                            <Text style={{ color: 'white', fontSize: 20 }}>
+                                Checkout
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </>
+    );
+
     return (
         <>
+            <Modal
+                animationType="slide"
+                visible={modalVisible}
+                transparent={true}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                {checkoutModalContent()}
+            </Modal>
             {total ? (
                 <View
                     style={{
@@ -47,6 +131,7 @@ const ViewCart = () => {
                                 width: 300,
                                 position: 'relative',
                             }}
+                            onPress={() => setModalVisible(true)}
                         >
                             <Text
                                 style={{
